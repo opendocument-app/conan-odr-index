a37f5c00daca358e55d6b89c8b13b8dc2ebdd858
Andreas Stefl <stefl.andreas@gmail.com>
2024-05-25 09:23:55 +0200

    try generalize CI

---
+++ scripts/list_package_versions.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+import argparse
+from pathlib import Path
+import yaml
+
+def main():
+    parser = argparse.ArgumentParser(description="List package versions")
+    parser.add_argument("--github", help="Format output for GitHub Actions", action="store_true")
+    args = parser.parse_args()
+
+    script_path = Path(__file__).resolve().parent
+    root_path = script_path.parent
+    recipes_path = root_path / "recipes"
+
+    package_infos = {}
+
+    for recipe_path in recipes_path.iterdir():
+        if not recipe_path.is_dir():
+            continue
+
+        infos = []
+
+        with open(recipe_path / "config.yml") as f:
+            config = yaml.safe_load(f)
+
+        for version, details in config["versions"].items():
+            infos.append({
+                "version": version,
+                "folder": Path(details["folder"]),
+            })
+
+        package_infos[recipe_path.name] = infos
+
+    if args.github:
+        result = [
+            {
+                "package_version": f"{package}/{infos["version"]}",
+                "package": package,
+                "version": infos["version"],
+                "conanfile": str(Path("recipes") / package / infos["folder"] / "conanfile.py"),
+            } for package in sorted(package_infos.keys())
+              for infos in sorted(package_infos[package], key=lambda x: x["version"])
+        ]
+
+        print(result)
+    else:
+        for package in sorted(package_infos.keys()):
+            infos = package_infos[package]
+            print(f"{package}: {", ".join(sorted(info['version'] for info in infos))}")
+
+if __name__ == "__main__":
+    main()
