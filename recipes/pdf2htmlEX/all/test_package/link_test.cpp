12d845af06f50b56c03398088a8a4d0505341a22
Vilius Sutkus '89 <Vilius@ViliusSutkus89.com>
2024-06-17 22:16:02 +0300

    Update pdf2htmlEX test_package

---
+++ recipes/pdf2htmlEX/all/test_package/link_test.cpp
@@ -0,0 +1,7 @@
+#include "pdf2htmlEX.h"
+
+int main()
+{
+    pdf2htmlEX::pdf2htmlEX converter;
+    return 0;
+}
--- recipes/pdf2htmlEX/test_package/src/link_test.c
+++
@@ -1,8 +0,0 @@
-//#include <fontforge.h>
-//#include "start.h"
-
-int main()
-{
-//    InitSimpleStuff();
-    return 0;
-}
