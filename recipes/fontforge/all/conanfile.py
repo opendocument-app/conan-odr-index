e420273df8a1b6983dbf1c071506eba11a0e3296
Vilius Sutkus '89 <Vilius@ViliusSutkus89.com>
2024-06-17 17:04:34 +0300

    fontforge: add patch to handle getting user's HOME dir

--- recipes/fontforge/all/conanfile.py
+++ recipes/fontforge/all/conanfile.py
@@ -117,13 +117,6 @@ class FontForgeConan(ConanFile):
         #     patch(self, patch_file=os.path.join(patch_folder, "pie.patch"))
         # patch(self, patch_file=os.path.join(patch_folder, "FindGLib.patch"))
 
-        # android-ndk-r20/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/include/pwd.h
-        # #if __ANDROID_API__ >= 26
-        # struct passwd* getpwent(void) __INTRODUCED_IN(26);
-        # void setpwent(void) __INTRODUCED_IN(26);
-        # void endpwent(void) __INTRODUCED_IN(26);
-        # #endif /* __ANDROID_API__ >= 26 */
-        # patch(self, patch_file=os.path.join(patch_folder, "gutils-fsys.patch"))
 
         # patch(self, patch_file=os.path.join(patch_folder, "InstallLibrary.patch"))
 
