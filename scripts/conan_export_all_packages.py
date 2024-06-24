37bb728fd74ee8210c3a584235c63ae624219baa
Vilius Sutkus '89 <Vilius@ViliusSutkus89.com>
2024-06-25 01:05:35 +0300

    Update conan_export_all_packages to properly handle paths when started from somewhere else than the root repository dir

--- scripts/conan_export_all_packages.py
+++ scripts/conan_export_all_packages.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
-import os
 import subprocess
+from pathlib import Path
 
 from list_package_versions import get_package_infos
 
 
 def main():
+    script_path = Path(__file__).resolve().parent
+    root_path = script_path.parent
+    recipes_path = root_path / "recipes"
     package_infos = get_package_infos()
     for package in package_infos:
         for version in package_infos[package]:
-            conanfile = os.path.join("recipes", package, version["folder"], "conanfile.py")
+            conanfile = recipes_path / package / version["folder"] / "conanfile.py"
             subprocess.run(["conan", "export", conanfile, "--version", version["version"]])
 
 
