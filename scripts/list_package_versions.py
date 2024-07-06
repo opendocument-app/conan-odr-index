58e4353afd9f663a68e389ef18b5dbcba224c28b
Vilius Sutkus '89 <Vilius@ViliusSutkus89.com>
2024-07-07 00:09:31 +0300

    update dependency tree parsing logic

--- scripts/list_package_versions.py
+++ scripts/list_package_versions.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 import argparse
 import json
+import os
 import subprocess
-from pathlib import Path, PurePath
+from pathlib import Path
 
 import yaml
 
@@ -36,90 +37,118 @@ def get_package_infos():
     return package_infos
 
 
-def main():
-    parser = argparse.ArgumentParser(description="List package versions")
-    parser.add_argument("--github", help="Format output for GitHub Actions", action="store_true")
-    parser.add_argument("--git-modified", nargs='*', dest="COMMIT_ID",
-                        help="Return packages modified by supplied commits")
-    parser.add_argument("--modified-tree", nargs='+', dest="CONAN_GRAPH.json",
-                        help="Extra parameter for --git-modified to include downstream dependants of modified "
-                             "packages too")
-    args = parser.parse_args()
+def get_files_in_commit(commit_id):
+    files_in_commit = subprocess.run(
+        ["git", "diff-tree", "--no-commit-id", "--name-only", "-r", commit_id],
+        capture_output=True,
+        text=True,
+        cwd=root_path,
+    )
+    return filter(None, files_in_commit.stdout.split("\n"))
 
-    if getattr(args, 'CONAN_GRAPH.json') and args.COMMIT_ID is None:
-        raise Exception("--modified-tree can only be used with --git-modified")
 
-    package_infos = get_package_infos()
+def get_packages_in_commit(commit_id):
+    packages = set()
+    for file_in_commit in get_files_in_commit(commit_id):
+        file_components = file_in_commit.split(os.sep)
+        if len(file_components) >= 3 and file_components[0] == 'recipes':
+            packages.add(file_components[1])
+    return packages
 
-    if args.COMMIT_ID:
-        updated_packages = set()
 
-        for commit_id in args.COMMIT_ID:
-            files_in_commit = subprocess.run(
-                ["git", "diff-tree", "--no-commit-id", "--name-only", "-r", commit_id],
-                capture_output=True,
-                text=True,
-                cwd=root_path,
-            )
-            for updated_file in filter(None, files_in_commit.stdout.split("\n")):
-                update_file_full_path = PurePath(root_path / updated_file)
-                if update_file_full_path.is_relative_to(recipes_path):
-                    updated_recipe_name = update_file_full_path.parts[len(recipes_path.parts)]
-                    updated_packages.add(updated_recipe_name)
+def get_downstream_dependencies(dependency_graph_files, package_infos):
+    # downstream_packages dictionary key is package name,
+    # value is a set of packages, which use this package
+    downstream_packages = dict()
+    for conan_dependency_graph_file in dependency_graph_files:
+        print("Parsing dependencies in " + conan_dependency_graph_file)
+        with open(conan_dependency_graph_file, 'r') as dep_json:
+            nodes = json.load(dep_json)['graph']['nodes']
+            for node in nodes:
+                node_name = nodes[node]['name']
+                if node_name not in package_infos:
+                    continue
 
-        # downstream_packages dictionary key is package name,
-        # value is a set of packages, which use this package
-        downstream_packages = dict()
-        for conan_dependency_graph_file in getattr(args, 'CONAN_GRAPH.json'):
-            with open(conan_dependency_graph_file, 'r') as dep_json:
-                nodes = json.load(dep_json)['graph']['nodes']
-                for node in nodes:
-                    node_name = nodes[node]['name']
-                    if node_name not in package_infos:
+                for dependency in nodes[node]['dependencies']:
+                    dependency_ref = nodes[node]['dependencies'][dependency]['ref']
+                    dep_name = dependency_ref.split('/')[0]
+
+                    # Not tracking external dependencies
+                    if dep_name not in package_infos:
                         continue
 
-                    for dependency in nodes[node]['dependencies']:
-                        dependency_ref = nodes[node]['dependencies'][dependency]['ref']
-                        dep_name = dependency_ref.split('/')[0]
+                    if dep_name not in downstream_packages:
+                        downstream_packages[dep_name] = set()
 
-                        if dep_name not in package_infos:
-                            continue
+                    downstream_packages[dep_name].add(node_name)
+    return downstream_packages
 
-                        if dep_name not in downstream_packages:
-                            downstream_packages[dep_name] = set()
 
-                        downstream_packages[dep_name].add(node_name)
+def main():
+    parser = argparse.ArgumentParser(description="List package versions")
+    parser.add_argument("--commit-ids", nargs='*', dest="COMMIT_ID",
+                        help="Find packages modified by supplied commits. Commit ids will also be obtained from "
+                             "$ENV[GITHUB_EVENT][commits]")
+    parser.add_argument("--requested-package", action='store',
+                        help="Requested package will also be obtained from "
+                             "$ENV[GITHUB_EVENT][inputs][package_name]")
+    parser.add_argument("--requested-package-version", action='store',
+                        help="Used together with --requested-package, ignored when building all packages. "
+                             "Requested package version will also be obtained from "
+                             "$ENV[GITHUB_EVENT][inputs][package_version]")
+    parser.add_argument("--dependency-graph", nargs='+', dest="CONAN_DEPENDENCY_GRAPH.json",
+                        help="Used to calculate downstream dependents of requested packages")
 
-        filtered_packages = {}
-        for package in package_infos:
-            if package in updated_packages:
-                filtered_packages[package] = package_infos[package]
-                for dependency in downstream_packages.get(package, list()):
-                    filtered_packages[dependency] = package_infos[dependency]
-        package_infos = filtered_packages
+    args = parser.parse_args()
 
-    if args.github:
-        result = [
-            {
-                "package_reference": f"{package}/{infos["version"]}",
-                "package": package,
-                "version": infos["version"],
-                "conanfile": str(
-                    Path("recipes") / package / infos["folder"] / "conanfile.py"
-                ),
-                "test_conanfile": str(
-                    Path("recipes") / package / infos["folder"] / "test_package" / "conanfile.py"
-                )
-            }
-            for package in sorted(package_infos.keys())
-            for infos in sorted(package_infos[package], key=lambda x: x["version"], reverse=True)
-        ]
+    github_event = json.loads(os.environ.get('GITHUB_EVENT', '{}'))
+    inputs = github_event.get("inputs", dict())
 
-        print("packages=" + json.dumps(result))
+    requested_packages = set()
+
+    # Check if any packages were modified in git commits
+    for commit_id in args.COMMIT_ID or list():
+        print("commit {} requested as argument". format(commit_id))
+        requested_packages.add(get_packages_in_commit(commit_id))
+
+    for commit in github_event.get("commits", list()):
+        if not str.lower(commit["message"]).find('[skipci]'):
+            print("Found commit without [skipci]: {} - {}".format(commit["id"], commit["message"]))
+            requested_packages.add(get_packages_in_commit(commit["id"]))
+
+    # Check if any packages were asked to be rebuilt
+    if args.requested_package:
+        print("Requested package: " + args.requested_package)
+        if args.requested_package_version and args.requested_package != 'all':
+            requested_packages.add("{}/{}".format(args.requested_package, args.requested_package_version))
+        else:
+            requested_packages.add(args.requested_package)
+    elif inputs.get('package_name'):
+        print("Requested package: " + inputs.get('package_name'))
+        if inputs.get('package_version') and inputs.get('package_name') != 'all':
+            requested_packages.add("{}/{}".format(inputs.get('package_name'), inputs.get('package_version')))
+        else:
+            requested_packages.add(inputs.get('package_name'))
+
+    package_infos = get_package_infos()
+    filtered_packages = {}
+
+    if 'all' in requested_packages:
+        filtered_packages = package_infos
     else:
-        for package in sorted(package_infos.keys()):
-            infos = package_infos[package]
-            print(f"{package}: {", ".join(sorted(info['version'] for info in infos))}")
+        dependency_graph_files = getattr(args, 'CONAN_DEPENDENCY_GRAPH.json') or list()
+        downstream_deps = get_downstream_dependencies(dependency_graph_files, package_infos)
+        for package in requested_packages:
+            filtered_packages[package] = package_infos[package]
+            for dependency in downstream_deps.get(package, list()):
+                filtered_packages[dependency] = package_infos[dependency]
+
+    gh_output = os.environ.get('GITHUB_OUTPUT')
+    if gh_output:
+        with open(gh_output, 'w') as out:
+            print("packages=" + json.dumps(filtered_packages), file=out)
+
+    print("packages=" + ' '.join(filtered_packages))
 
 
 if __name__ == "__main__":
