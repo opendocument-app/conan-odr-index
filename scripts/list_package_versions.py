be33850cffa72bf115f9b8132c17579dc1f57234
Vilius Sutkus '89 <Vilius@ViliusSutkus89.com>
2024-08-13 17:56:03 +0300

    Add cron job, every 5 minutes, for testing

--- scripts/list_package_versions.py
+++ scripts/list_package_versions.py
@@ -153,6 +153,9 @@ def main():
         requested_packages[package].add(get_latest_package_version(package_infos, package))
     del commit_ids, commit_obj_list
 
+    if github_event.get("schedule", False):
+        requested_packages['all'] = 'newest'
+
     # Check if any packages were asked to be rebuilt
     input_requested_package = args.request_package if args.request_package else inputs.get('package_name')
     input_requested_version = args.request_package_version or inputs.get('package_version', 'newest')
