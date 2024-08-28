3974a2ba509c9064c42ba238c9eedc5d87fc9ca0
Vilius Sutkus '89 <Vilius@ViliusSutkus89.com>
2024-08-29 00:14:59 +0300

    Expose POPPLER_DATA_DIR path as a variable in runenv. Previously it was buildenv

--- recipes/poppler-data/all/conanfile.py
+++ recipes/poppler-data/all/conanfile.py
@@ -54,4 +54,4 @@ class PopplerDataConan(ConanFile):
         self.cpp_info.bindirs = []
 
         poppler_data_dir = os.path.join(self.package_folder, "res", "share", "poppler")
-        self.buildenv_info.define_path("POPPLER_DATA_DIR", poppler_data_dir)
+        self.runenv_info.define_path("POPPLER_DATA_DIR", poppler_data_dir)
