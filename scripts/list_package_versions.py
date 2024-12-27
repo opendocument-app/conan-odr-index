3d2167a871466d9c52ac4e36d50c4fcbd614dad2
Andreas Stefl <stefl.andreas@gmail.com>
2024-12-27 18:05:16 +0100

    simplify package listing

--- scripts/list_package_versions.py
+++ scripts/list_package_versions.py
@@ -7,6 +7,7 @@ import subprocess
 import sys
 from pathlib import Path
 from functools import cmp_to_key
+import fnmatch
 
 import yaml
 import semver
@@ -17,21 +18,6 @@ root_path = script_path.parent
 recipes_path = root_path / "recipes"
 default_packages_path = root_path / "default-packages-list.txt"
 TIER_COUNT = 5
-
-
-def get_default_packages():
-    with open(default_packages_path) as f:
-        list = f.read().splitlines()
-
-    result = {}
-
-    for package_version in list:
-        package, version = package_version.split("/")
-        if package not in result:
-            result[package] = set()
-        result[package].add(version)
-
-    return result
 
 
 def get_package_infos():
@@ -79,6 +65,31 @@ def get_package_infos():
     return package_infos
 
 
+def get_package_references(package_infos):
+    return [
+        package_info["package_reference"]
+        for package_infos in package_infos.values()
+        for package_info in package_infos
+    ]
+
+
+def get_selected_packages(package_references, config):
+    with open(config) as f:
+        selection = yaml.safe_load(f)
+    package_selection = selection["packages"]
+
+    result = []
+
+    for package_reference in package_references:
+        if all(not fnmatch.fnmatch(package_reference, pattern) for pattern in package_selection.get("include", ["*"])):
+            continue
+        if any(fnmatch.fnmatch(package_reference, pattern) for pattern in package_selection.get("exclude", [])):
+            continue
+        result.append(package_reference)
+
+    return result
+
+
 def get_package_info(package_infos, package, version):
     versions = package_infos[package]
     for package_versioned in versions:
@@ -114,94 +125,13 @@ def get_modified_packages_in_commits(commit_id_list):
     return packages
 
 
-def get_downstream_dependents(dependency_graph_files, package_infos):
-    # downstream_packages dictionary key is package name,
-    # value is a set of packages, which use this package
-    downstream_packages = {}
-    for conan_dependency_graph_file in dependency_graph_files:
-        print("Parsing dependencies in " + conan_dependency_graph_file)
-        with open(conan_dependency_graph_file, "r") as dep_json:
-            nodes = json.load(dep_json)["graph"]["nodes"]
-            for node in nodes:
-                node_name = nodes[node]["name"]
-                if node_name not in package_infos:
-                    continue
-
-                for dependency in nodes[node]["dependencies"]:
-                    dependency_ref = nodes[node]["dependencies"][dependency]["ref"]
-                    dep_name = dependency_ref.split("/")[0]
-
-                    # Not tracking external dependencies
-                    if dep_name not in package_infos:
-                        continue
-
-                    if dep_name not in downstream_packages:
-                        downstream_packages[dep_name] = set()
-
-                    downstream_packages[dep_name].add(node_name)
-    return downstream_packages
-
-
-def get_latest_package_version(package_infos, package_name):
-    return package_infos[package_name][0]["version"]
-
-
-def add_dependents_to_requested_packages(
-    requested_packages, downstream_dependents, package_infos
-):
-    dependents = set()
-    for package in requested_packages.keys():
-        for dependant in downstream_dependents.get(package, []):
-            dependents.add(dependant)
-    for dependant in dependents:
-        if dependant not in requested_packages.keys():
-            requested_packages[dependant] = set()
-        requested_packages[dependant].add(
-            get_latest_package_version(package_infos, dependant)
-        )
-
-
-def get_tiered_packages(requested_packages, downstream_dependents, package_infos):
-    tiered_packages = [requested_packages]
-    dependency_tier = 0
-    while dependency_tier < len(tiered_packages):
-        packages_from_this_tier = dict(tiered_packages[dependency_tier])
-        packages_to_add_in_next_tier = {}
-        for package in packages_from_this_tier.keys():
-            for dependant in downstream_dependents.get(package, []):
-                if dependant in packages_from_this_tier.keys():
-                    packages_to_add_in_next_tier[dependant] = packages_from_this_tier[
-                        dependant
-                    ]
-                    if dependant in tiered_packages[dependency_tier].keys():
-                        del tiered_packages[dependency_tier][dependant]
-        if len(packages_to_add_in_next_tier):
-            tiered_packages.append(packages_to_add_in_next_tier)
-        dependency_tier += 1
-
-    if dependency_tier > TIER_COUNT:
-        this_file = Path(__file__).resolve().relative_to(root_path)
-        print(
-            f"Dependency tier overflow. Increase TIER_COUNT in {this_file} and in .github/workflows/build.yml",
-            file=sys.stderr,
-        )
-        sys.exit(1)
-
-    result = [[] for _ in range(TIER_COUNT)]
-    for tier, packages in enumerate(tiered_packages):
-        package_infos_in_this_tier = []
-        for package in packages.keys():
-            for version in packages[package]:
-                package_infos_in_this_tier.append(
-                    get_package_info(package_infos, package, version)
-                )
-        result[tier] = package_infos_in_this_tier
-
-    return result
-
-
 def get_cli_args():
     parser = argparse.ArgumentParser(description="List package versions")
+    parser.add_argument(
+        "--selection-config",
+        type=Path,
+        help="Path to selection config file",
+    )
     parser.add_argument(
         "--commit-id",
         nargs="*",
@@ -234,6 +164,8 @@ def get_github_args():
     event = json.loads(os.environ.get("GITHUB_EVENT", "{}"))
     inputs = event.get("inputs", {})
 
+    selection_config = root_path / "default.yml"
+
     commit_obj_list = event.get("commits", [])
     commit_ids = [
         commit["id"]
@@ -251,6 +183,7 @@ def get_github_args():
         request_package = "default"
 
     return argparse.Namespace(
+        selection_config=selection_config,
         commit_id=commit_ids,
         request_package=request_package,
         request_package_version=request_package_version,
@@ -272,73 +205,27 @@ def main():
         args = get_cli_args()
 
     package_infos = get_package_infos()
-    default_packages = get_default_packages()
-    requested_packages = {}
+    package_references = get_package_references(package_infos)
+    if args.selection_config is not None:
+        selected_packages = get_selected_packages(package_references, args.selection_config)
+    else:
+        selected_packages = package_references
 
-    for package in get_modified_packages_in_commits(args.commit_id):
-        if package not in requested_packages.keys():
-            requested_packages[package] = set()
-        requested_packages[package].add(
-            get_latest_package_version(package_infos, package)
-        )
+    if args.commit_id:
+        newly_selected_packages = []
+        modified_packages = get_modified_packages_in_commits(args.commit_id)
+        for package_references in selected_packages:
+            if package_references.split("/")[0] in modified_packages:
+                newly_selected_packages.append(package_references)
+        selected_packages = newly_selected_packages
 
-    # Check if any packages were asked to be rebuilt
-    input_requested_package = args.request_package
-    input_requested_version = args.request_package_version
-    if (
-        input_requested_package is not None
-        and input_requested_package != "default"
-        and input_requested_package not in package_infos.keys()
-    ):
-        print(
-            f"Requested package {input_requested_package} not found in recipes",
-            file=sys.stderr,
-        )
-        return 1
-    if input_requested_package == "default":
-        for package, versions in default_packages.items():
-            if package not in requested_packages.keys():
-                requested_packages[package] = set()
-            requested_packages[package].update(versions)
-    elif input_requested_package is not None:
-        print(f"Requested package: {input_requested_package}/{input_requested_version}")
-        if input_requested_package not in requested_packages.keys():
-            requested_packages[input_requested_package] = set()
-        if input_requested_version == "latest":
-            requested_packages[input_requested_package].add(
-                get_latest_package_version(package_infos, input_requested_package)
-            )
-        elif input_requested_version == "all":
-            for version in list(
-                map(lambda v: v["version"], package_infos[input_requested_package])
-            ):
-                requested_packages[input_requested_package].add(version)
-        else:
-            requested_packages[input_requested_package].add(input_requested_version)
-
-    dependency_graph_files = args.dependency_graph or []
-    downstream_dependents = get_downstream_dependents(
-        dependency_graph_files, package_infos
-    )
-
-    add_dependents_to_requested_packages(
-        requested_packages, downstream_dependents, package_infos
-    )
-
-    tiered_packages = get_tiered_packages(
-        requested_packages, downstream_dependents, package_infos
-    )
-
-    for tier_index, packages_in_tier in enumerate(tiered_packages):
-        print(
-            f"packages_{tier_index}="
-            + " ".join(map(lambda x: x["package_reference"], packages_in_tier))
-        )
+    for package_reference in selected_packages:
+        print(package_reference)
 
     if is_github:
         with open(args.github_output, "w") as out:
-            for tier_index, packages_in_tier in enumerate(tiered_packages):
-                print(f"packages_{tier_index}={json.dumps(packages_in_tier)}", file=out)
+            for package_reference in selected_packages:
+                print(f"packages={json.dumps(package_reference)}", file=out)
 
     return 0
 
