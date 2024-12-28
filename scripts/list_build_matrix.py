045990d2149a7c80f5e2265b373b7a7413ee8c99
Andreas Stefl <stefl.andreas@gmail.com>
2024-12-28 12:02:22 +0100

    fix path

--- scripts/list_build_matrix.py
+++ scripts/list_build_matrix.py
@@ -191,7 +191,7 @@ def get_github_args():
     github = json.loads(os.environ.get("GITHUB_CONTEXT", "{}"))
     inputs = json.loads(os.environ.get("GITHUB_INPUT", "{}"))
 
-    conanfile = inputs.get("conanfile")
+    conanfile = Path(inputs.get("conanfile"))
     version = inputs.get("package_version")
 
     selection_config = root_path / "defaults.yaml"
