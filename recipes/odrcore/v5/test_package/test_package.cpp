83ac9a18bb6cc6553d4ae1c47ba1f14539f37872
Andreas Stefl <stefl.andreas@gmail.com>
2025-07-02 20:50:50 +0200

    fix odrcore v5 recipe and test package

--- recipes/odrcore/v5/test_package/test_package.cpp
+++ recipes/odrcore/v5/test_package/test_package.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 
 int main(void) {
-    std::cout << "odr version=\"" << odr::get_version() << "\' "
-              << "commit=\'" << odr::get_commit() << "\""
+    std::cout << "odr version=\"" << odr::version() << "\' "
+              << "commit=\'" << odr::commit_hash() << "\""
               << std::endl;
 }
