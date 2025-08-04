eb6c7a81eafc2d1999635742dc096a9c4a756865
Andreas Stefl <stefl.andreas@gmail.com>
2025-08-04 22:08:27 +0200

    update lock files

---
+++ scripts/conan_lock_all_packages.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import sys
+import subprocess
+from pathlib import Path
+
+from list_package_references import get_package_infos
+
+
+def main():
+    returncode = 0
+
+    script_path = Path(__file__).resolve().parent
+    root_path = script_path.parent
+
+    package_infos = get_package_infos()
+    for package_name in package_infos:
+        for package_info in package_infos[package_name]:
+            print(
+                f"Lock package {package_name} version {package_info["version"]} ..."
+            )
+
+            proc = subprocess.run(
+                [
+                    "conan",
+                    "lock",
+                    "create",
+                    str(Path(package_info["directory"]) / package_info["conanfile"]),
+                    "--version",
+                    package_info["version"],
+                    "--profile:host",
+                    str(Path(".github/config/conan/profiles/android-21-armv8")),
+                ],
+                cwd=root_path,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
+                text=True,
+            )
+            if proc.returncode != 0:
+                print("... failed to export")
+                print(proc.stdout)
+                returncode = proc.returncode
+
+    return returncode
+
+
+if __name__ == "__main__":
+    sys.exit(main())
